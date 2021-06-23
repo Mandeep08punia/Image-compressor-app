9d4abdcd9db41f0240dc69984d14a0d5835492f9
Mandeep Punia <48849295+Mandeep08punia@users.noreply.github.com>
2021-06-23 21:23:11 +0530

    upload logic ✔

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
 const express=require('express')
 const bodyparser=require('body-parser')
 const multer=require('multer')
-const path=require('path')
+const path=require('path');
+const { nextTick } = require('process');
 const app=express();
 
 app.set('view engine','ejs');
@@ -25,6 +26,25 @@ const upload=multer({
 app.get('/',(req,res)=>{
     res.render('index')
 })
+app.post('/',upload.single('image'),(req,res,next)=>{
+    const file=req.file
+    let ext;
+    if(!file){
+        const error=new Error("Please upload a file")
+        error.httpStatusCode=404
+        return next(error)
+    }
+    if(file.mimetype=='image/jpeg'){
+        ext='jpg'
+    }
+    if(file.mimetype=='image/png'){
+        ext='png'
+    }
+    res.render('image.ejs',{url:file.path,name:file.filename,ext:ext})
+
+})
+
+
 
 app.listen(5000,function(){
     console.log("running on port 5000")
